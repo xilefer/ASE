1f1d325d84c79363c8c9a861431a634407d6408b
Felix Geiselhart <i14009@hb.dhbw-stuttgart.de>
2016-12-07 09:38:21 +0100

    no message

---
+++ Softwaretests/Events.js
@@ -0,0 +1,37 @@
+﻿var Controller = new ApplicationController(navigate);
+
+function updateLoginButtonEnabled() {
+    if (Controller.loginPossible()) {
+        $('#Loginbutton').prop("disabled", false);
+    } else {
+        $('#Loginbutton').prop("disabled", true);
+    }
+}
+
+$(document).on("pagebeforeshow", "#loginPage", function () {
+    updateLoginButtonEnabled();
+});
+$(document).on("pagebeforeshow", "#welcomepage", function () {
+    $('#welcomelabel').text("Willkommen: " + Controller.username);
+});
+$(function() {
+    $('#Loginbutton').click(function(e) {
+        e.preventDefault();
+        Controller.login();
+        navigate('#welcomepage');
+    });
+});
+$(function() {
+    $('#password').keyup(function () {
+        Controller.password = $('#password').val();
+        updateLoginButtonEnabled();
+    });
+});
+
+$(function () {
+    $('#username').keyup(function () {
+        Controller.username = $('#username').val();
+        updateLoginButtonEnabled();
+    });
+});
+
---
+++ Softwaretests/JavaScript.js
@@ -0,0 +1,22 @@
+﻿function ApplicationController(navigate) {
+    this.username = "";
+    this.password = "";
+    this.loginPossible = function () {
+        if (this.username.length === 0 || this.password.length === 0) {
+            return false;
+        }
+        else return true;
+    };
+    this.login = function () {
+        this.customer = {
+            name: this.username
+        };
+        navigate("#welcomepage");
+    };
+}
+
+var navigate = function(target) {
+    $.mobile.changePage(target);
+};
+
+
